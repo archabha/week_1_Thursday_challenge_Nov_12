4ba5dc32d9301663f9123697eceeb3d1949e89d2
chabha arkoub <ctazga@gmail.com>
2020-11-17 19:50:35 -0500

    initial commit

---
+++ Desktop/elevenfiftyProjects/DotNetPrejects/challenges/week_1_Thursday_challenge_Nov_12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace week_1_Thursday_challenge_Nov_12
+{
+    class Program
+    {
+        static void Main(string[] args) { }
+    }
+     
+    [TestClass]
+   public class week1
+
+    {
+        [TestMethod]
+        
+        public void superchal()
+
+      {
+        string name = "supercalifragilisticexpialidocious";
+        foreach (char letter in name)
+        {
+            Console.WriteLine(letter);
+        }
+
+      }
+
+
+    [TestMethod]
+    public void IfiChal()
+    {
+        string name = "supercalifragilisticexpialidocious";
+        foreach (char letter in name) 
+            if (letter == 'i')
+        {
+           
+            Console.WriteLine('i');
+        }
+
+        else
+            {
+                Console.WriteLine( "Not an i");
+            }
+
+        
+        
+            
+        
+    }
+
+    }
+
+}
