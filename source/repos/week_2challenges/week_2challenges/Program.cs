e70cd4a8da011910bd16bea23eb17cef22c905c9
chabha arkoub <ctazga@gmail.com>
2020-11-21 19:52:15 -0500

    initial commit

---
+++ source/repos/week_2challenges/week_2challenges/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_2challenges
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Greeter greeter = new Greeter();
+            greeter.DayTimeGreeting();
+            greeter.TimeGreeting();
+            greeter.Greeting("Ben");
+            greeter.Hello();
+            greeter.Farwell("Ben");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/repos/week_2challenges/week_2challenges/console_chall_week2-Greeter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_2challenges
+{ 
+        public class Greeter
+    {
+        public string Greeting(string name)
+        {
+            string greeting = $"Hello{name}";
+            return greeting;
+        }
+
+        internal void DayTimeGreeting()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void TimeGreeting()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hello()
+        {
+            string inputName = Greeting("Josh");
+            Console.WriteLine(inputName);
+        }
+        public string Farwell(string name)
+        {
+            string farwell = $"It was nice to meet you {name}";
+            return farwell;
+        }
+
+        public void Goodby()
+        {
+            string inputName = Farwell("Josh");
+            Console.WriteLine(inputName);
+        }
+    }
+}
