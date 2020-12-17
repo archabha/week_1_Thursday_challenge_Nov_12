5bd92dd8dad713a97e7d23b7e0750fa79418b60a
chabha arkoub <ctazga@gmail.com>
2020-12-17 17:30:42 -0500

    commit

---
+++ source/repos/solution 'GoldBadgeConsoleApplicationChallenge'(12 of 12 projects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solution__GoldBadgeConsoleApplicationChallenge__12_of_12_projects
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ source/repos/solution 'GoldBadgeConsoleApplicationChallenge'(12 of 12 projects/programUI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solution__GoldBadgeConsoleApplicationChallenge__12_of_12_projects
+{
+    internal class programUI
+    {
+    }
+}
